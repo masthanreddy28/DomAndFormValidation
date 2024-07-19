253bb171747648e93dc0e03ec7b8320d0cbcab42
masthanreddy28 <masthanreddy978@gmail.com>
2024-07-19 12:30:08 +0530

    DomAndFormValid

---
+++ DomJavascript.js
@@ -0,0 +1,106 @@
+
+
+// function getcube(){  
+//     var number=document.getElementById("number").value;  
+//     alert(number*number*number);  
+//     }  
+// function totalelements()  
+// {  
+// var allgenders=document.getElementsByName("gender");  
+// alert("Total Genders:"+allgenders.length);  
+// }  
+
+// function countpara(){  
+//     var totalpara=document.getElementsByTagName("p");  
+//     alert("total p tags are: "+totalpara.length);  
+      
+//     }  
+
+// function counth2(){  
+//     var totalh2=document.getElementsByTagName("h2");  
+//     alert("total h2 tags are: "+totalh2.length);  
+//     }  
+//     function counth3(){  
+//     var totalh3=document.getElementsByTagName("h3");  
+//     alert("total h3 tags are: "+totalh3.length);  
+//     }  
+// function showcommentform() {  
+//     var data="Name:<br><input type='text' name='name'><br>Comment:<br><textarea rows='5' cols='50'></textarea><br><input type='submit' value='comment'>";  
+      
+//     document.getElementById('mylocation').innerHTML=data;  
+//      }  
+
+
+function validateForm() {
+    const name = document.getElementById("name").value;
+    const address = document.getElementById("address").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const subject = document.getElementById("subject").value;
+    const agree = document.getElementById("agree").checked;
+    const nameError = document.getElementById("name-error");
+    
+    const addressError = document.getElementById(
+        "address-error"
+    );
+    const emailError = document.getElementById(
+        "email-error"
+    );
+    const passwordError = document.getElementById(
+        "password-error"
+    );
+    const subjectError = document.getElementById(
+        "subject-error"
+    );
+    const agreeError = document.getElementById(
+        "agree-error"
+    );
+
+    nameError.textContent = "";
+    addressError.textContent = "";
+    emailError.textContent = "";
+    passwordError.textContent = "";
+    subjectError.textContent = "";
+    agreeError.textContent = "";
+
+    let isValid = true;
+
+    if (name === "" || /\d/.test(name)) {
+        nameError.textContent =
+            "Please enter your name properly.";
+        isValid = false;
+    }
+
+    if (address === "") {
+        addressError.textContent =
+            "Please enter your address.";
+        isValid = false;
+    }
+
+    if (email === "" || !email.includes("@")) {
+        emailError.textContent =
+            "Please enter a valid email address.";
+        isValid = false;
+    }
+
+    if (password === "" || password.length < 6) {
+        passwordError.textContent =
+            "Please enter a password with at least 6 characters.";
+        isValid = false;
+    }
+
+    if (subject === "") {
+        subjectError.textContent =
+            "Please select your course.";
+        isValid = false;
+    }
+
+    if (!agree) {
+        agreeError.textContent =
+            "Please agree to the above information.";
+        isValid = false;
+    }
+
+    return isValid;
+}
+      
